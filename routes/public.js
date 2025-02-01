9d1880ed8865eb04111647fa6b7c3a4fa886d4a0
felipe <felipemoreno148@gmail.com>
2025-02-01 12:55:03 -0300

    criada rota  de cadastro POST

--- routes/public.js
+++ routes/public.js
@@ -1,5 +1,26 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client/extension";
 
-export default function PublicRoutes() {}
+router = express.Router();
+prisma = new PrismaClient();
 
+export default function PublicRoutes() {
+  router.post("/cadastro", async (req, res) => {
+    try {
+      const user = req.body;
+      await prisma.user.create({
+        data: {
+          email: user.email,
+          name: user.name,
+          password: user.password,
+          cpf: user.cpf,
+        },
+      });
+      res.status(200);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "erro no servidor, tente novamente mais tarde." });
+    }
+  });
+}
